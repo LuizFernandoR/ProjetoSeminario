d6c5254afb10d0464e6c5225275c84559cadcfda
Luiz Fernando Reichert <nandoreichert1506@gmail.com>
2021-06-07 21:34:16 -0300

    Projeto seminário

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include 'templates/header.html'; ?>
+
+
+<img class="img-fluid" src="static/img/banner1.jpg" alt="Banner">
+
+<h1>Clínica Viver Mais</h1>
+
+
+<?php include 'templates/footer.html'; ?>
+
+   
